674f34794d84d045b658ebb71304fed7250b44e5
Andy <babyandy0111@gmail.com>
2023-09-26 15:42:55 +0800

    init

--- lib/model/video_model.dart
+++ lib/model/video_model.dart
@@ -1,16 +1,26 @@
 import 'dart:io';
 
 Socket? socket;
+
 var videoList = [
-  'test-video-10.mp4',
-  'test-video-6.mp4',
-  'test-video-9.mp4',
-  'test-video-8.mp4',
-  'test-video-7.mp4',
-  'test-video-1.mp4',
-  'test-video-2.mp4',
-  'test-video-3.mp4',
-  'test-video-4.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-10.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-6.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-9.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-8.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-7.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-1.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-2.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-3.mp4',
+  'https://www.shihjie.com/video/test-video/test-video-4.mp4',
+  'https://sfux-ext.sfux.info/hls/chapter/105/1588724110/1588724110.m3u8',
+  'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8',
+  'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.mp4/.m3u8',
+  'https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8',
+  'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
+  'http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_stereo_subs.m3u8',
+  'http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_multichannel_subs.m3u8',
+  'http://d3rlna7iyyu8wu.cloudfront.net/skip_armstrong/skip_armstrong_multi_language_subs.m3u8',
+  'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
 ];
 
 class UserVideo {
@@ -25,13 +35,8 @@ class UserVideo {
   });
 
   static List<UserVideo> fetchVideo() {
-    List<UserVideo> list = videoList
-        .map((e) => UserVideo(
-            image: '',
-        // url: 'https://www.shihjie.com/video/test-video/$e',
-        url: 'https://sfux-ext.sfux.info/hls/chapter/105/1588724110/1588724110.m3u8',
-        desc: '$e'))
-        .toList();
+    List<UserVideo> list =
+        videoList.map((e) => UserVideo(image: '', url: e, desc: e)).toList();
     return list;
   }
 
